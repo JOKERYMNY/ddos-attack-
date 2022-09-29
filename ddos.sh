2dae393f4219fefcf68124bec0e2efdba2302479
Joker YM <112567764+JOKERYMNY@users.noreply.github.com>
2022-09-29 17:19:10 +0300

    Add files via upload

---
+++ ddos.sh
@@ -0,0 +1,17 @@
+#copyright : @qgi.p
+# DDoS Tools Copright : qgi.p
+#telegram : @@x7l77
+#we are no_team9
+
+clear
+figlet J O K E R  Y M  DDoS 
+echo -e "\e[3;84m[:::] ...::: DDoS Tools Copright : J O K E R   Y M                 :::... [:::]"
+echo -e "\e[2;52m[:::] ...::: github : https://github.com/hacker2008274        :::... [:::]"
+echo -e "\e[3;48m[:::] ...::: telegram : https://t.me/Al_ymni               :::... [:::]" 
+echo -e "\033[3;92m[:::]...::: Instagram :    [ qgi.p  ]                       :::... [:::]"
+echo -e "\e[1;34m[:::] Target (example inter the URL here : https://url.com/)  : "
+echo -e "\e[1;34m ===> "
+read target
+echo -e "\033[3;93m[:::]         I am J O k E R  Y M         [:::]"
+sleep 5
+go run 1.go -site $target
